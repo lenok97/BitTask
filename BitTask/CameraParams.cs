1c4ef250f34aa3fdc44ba82003d422e376a51441
lenok97 <iamhappluck@gmail.com>
2021-09-02 23:08:40 +0500

    Class to calculate camera vertical angle

---
+++ BitTask/CameraParams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BitTask
+{ 
+	public class CameraParams
+	{
+		private float averageHumanHeight = 160;
+
+        public CameraParams(float distance, float cameraHeight)
+        {
+            Distance = ValidateParam(distance);
+            CameraHeight = ValidateParam(cameraHeight);
+        }
+
+        public long Id { get; set; }
+
+		public float Distance { get; set; }
+
+        public float CameraHeight { get; set; }
+
+        public float B 
+        { get => 
+			Math.Abs(CameraHeight - averageHumanHeight); }
+
+        public float Angle
+		{ get => 
+				(float)((180 / Math.PI) * Math.Atan(B / Distance));}
+
+		private float ValidateParam(float param)
+        {
+			if (param <= 0)
+				throw new ArgumentException("Parameter should be > 0");
+			return param;
+        }
+	}
+}
